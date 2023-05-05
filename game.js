32408b0730d8c49c6046b33ac41d4601897a6855
ale vel <ale.vrs@outlook.com>
2023-05-04 20:29:40 -0500

    3. Medidas del canvas y sus elementos

--- game.js
+++ game.js
@@ -8,7 +8,36 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame(){
-    // game.fillRect( xinicial, yinicial, width, height)
+    // Medidas del canvas
+    let canvasSize;
+
+    if(window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    }else{
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    // Renderizar Bomba
+    const elementsSize = (canvasSize / 10) - 1; //10%
+    console.log({ canvasSize, elementsSize});
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = "";
+
+    for (let i = 0; i < 10; i++) {
+        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    }
+    
+
+    /* game.fillText(emojis['X'], 100, 100); */
+
+    /* 
+    window.innerHeight
+    window.innerWidth
+    game.fillRect( xinicial, yinicial, width, height)
     game.fillRect(145,0.1,20,20);
     game.fillRect(135,110,40,40);
     game.clearRect(145,0.1,10,10);
@@ -17,5 +46,5 @@ function startGame(){
     game.font = '25px Verdana';
     game.fillStyle = 'purple';
     game.textAlign = 'end';
-    game.fillText('Platzi', 100, 100)
+    game.fillText('Platzi', 180, 80); */
 }
